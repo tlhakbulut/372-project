b33d750d7af7a935e35980efa29043c7c0b9aec1
JoumanHmeid <105748339+JoumanHmeid@users.noreply.github.com>
2024-08-08 15:03:26 +0400

    Update forms.py

--- recipes/forms.py
+++ recipes/forms.py
@@ -19,3 +19,14 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name', 'prep_time', 'cook_time', 'total_time', 'servings', 'ingredients', 'directions', 'rating', 'url', 'cuisine_path', 'calories', 'protein', 'fat']
+
+
+class RecipeFilterForm(forms.Form):
+    
+    min_prep_time = forms.IntegerField(required=False, label='Minimum Hazırlama Süresi (dakika)')
+    max_prep_time = forms.IntegerField(required=False, label='Maksimum Hazırlama Süresi (dakika)')
+    min_total_time = forms.IntegerField(required=False, label='Minimum Toplam Süresi (dakika)')
+    max_total_time = forms.IntegerField(required=False, label='Maksimum Toplam Süresi (dakika)')
+    ingredients = forms.CharField(required=False, label='Malzemeler', widget=forms.Textarea)
+    min_rating = forms.FloatField(required=False, label='Minimum Puan')
+    max_rating = forms.FloatField(required=False, label='Maksimum Puan')
